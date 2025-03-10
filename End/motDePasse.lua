d6cfbd433d6b83cbb423ad66f97384018c549056
Emerick <decdu59@live.fr>
2025-03-10 20:51:12 +0100

    fix(motDePasse.lua): update system motdepasse

--- End/motDePasse.lua
+++ End/motDePasse.lua
@@ -1,13 +1,16 @@
-local caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
-local test = ""
 
-function generateur (longueur)
-    for i = 1, longueur do 
-        local aleatoire = math.random(1, #caracteres)
-         test = test .. caracteres:sub(aleatoire, aleatoire)
-    end 
+local caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+
+function generateur(longueur)
+    local test = ""  
+    for i = 1, longueur do
+        local aleatoire = math.random(1, #caracteres) 
+        test = test .. caracteres:sub(aleatoire, aleatoire)     
+    end
     return test
 end
 print(generateur(12))
+
+
 
 
