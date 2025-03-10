811b73a339217d9716af6134846ef65217faf8c4
Emerick <decdu59@live.fr>
2025-03-10 23:16:24 +0100

    update(MotDePasse): add ASCII in parameter password

--- motDePasse.lua
+++ motDePasse.lua
@@ -1,13 +1,21 @@
 local caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 local caracteresSpeciaux = "!@#$%^&*()"
+local caracteresASCII = "☺☻♥♦♣♠•◘○"
+
+-- permet un mot de passe différent a chaque relance du code 
+math.randomseed(os.time()) 
 
 -- genere un mot de passe aléatoire
-function generateur(longueur, speciaux)
+function generateur(longueur, speciaux, ascii)
     local selectionCaratere = caracteres
-    if speciaux then 
+    if speciaux and ascii then 
+        selectionCaratere = caracteres .. caracteresSpeciaux .. caracteresASCII
+    elseif speciaux and not ascii then 
         selectionCaratere = caracteres .. caracteresSpeciaux
+    elseif not speciaux and ascii then
+        selectionCaratere = caracteres .. caracteresASCII
     end
-
+   
     local MotDePasse = ""  
     for i = 1, longueur do
         local aleatoire = math.random(1, #selectionCaratere) 
@@ -15,24 +23,28 @@ function generateur(longueur, speciaux)
     end
     return MotDePasse
 end
--- genere le mot de passe
-local mdp = generateur(12, true)  
+-- paramètre du mot de passe
+local mdp = generateur(12, true, true)  
+local mdp2 = generateur(50, true, true)
+local mdp3 = generateur(100, true , true)
+print(mdp3)
 
 -- fonction qui verifie les critère demander 
 function verifMotDePasse(MotDePasse)
     local contientMajuscule = MotDePasse:match("%u") ~= nil  
     local contientMinuscule = MotDePasse:match("%l") ~= nil  
     local contientSpeciaux = MotDePasse:match("[!@#$%^&*()]") ~= nil  
+    local contientAscii = MotDePasse:match("[☺☻♥♦♣♠•◘○]") ~= nil  
     local contientChiffre = MotDePasse:match("%d") ~= nil  
     local pasDeSuite = not MotDePasse:match("123") and not MotDePasse:match("abc")
 
-    return contientMajuscule and contientMinuscule and contientSpeciaux and contientChiffre and pasDeSuite
+    return contientMajuscule and contientMinuscule and contientSpeciaux and contientChiffre and pasDeSuite and contientAscii
 end
 
 if verifMotDePasse(mdp) then
-    print("Mot de passe avec spéciaux valide")
+    print("Mot de passe valide")
 else
-    print("Mot de passe avec spéciaux invalide")
+    print("Mot de passe invalide")
 end
 
 -- calcul a revoir mais affiche un score aux mot de passe 
@@ -42,11 +54,13 @@ function scoreMotDePasse(MotDePasse)
    if MotDePasse:match("%l") then score = score +1 end
    if MotDePasse:match("[!@#$%^&*()]") then score = score +1 end
    if MotDePasse:match("%d") then score = score + 1 end
+   if MotDePasse:match("☺☻♥♦♣♠•◘○") then score = score +1 end
    if #MotDePasse >= 12 then score = score +2 end
     return score 
 end
- local score = scoreMotDePasse(mdp)
+ local score = scoreMotDePasse(mdp2)
  print(score)
+
 
 
 
